fed50ac5852f5ac64c32de8596c73a01397790e1
Engin Bolat <engi.bolat@gmail.com>
2022-06-21 12:57:32 +0300

    App Colors

---
+++ lib/constants/const_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConstColors {
+  static Color lightBlue = const Color.fromRGBO(179, 217, 236, 1);
+  static Color lightYellow = const Color.fromRGBO(237, 247, 116, 1);
+  static Color lightGreen = const Color.fromRGBO(144, 206, 33, 1);
+  static Color darkGreen = const Color.fromRGBO(60, 160, 59, 1);
+
+  static Color glassContainerColorOppacity01 =
+      const Color(0xFFffffff).withOpacity(0.1);
+  static Color glassContainerColorOppacity05 =
+      const Color(0xFFFFFFFF).withOpacity(0.05);
+  static Color glassContainerColorWhite05 =
+      const Color(0xFFffffff).withOpacity(0.5);
+}
